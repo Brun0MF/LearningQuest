9764dcbec3eea85d03df5517ba1985eed4ba011c
Catarina Costa <catarinac.2005@gmail.com>
2025-10-25 22:20:45 +0100

    teste

--- frontend/src/views/pages/login/login.jsx
+++ frontend/src/views/pages/login/login.jsx
@@ -2,8 +2,8 @@
 
 const Login = () => {
     return (
-        <div className="min-h-screen grid place-items-center bg-blue-100">
-            <h1 className="text-3xl font-bold text-blue-600">Tailwind a bombar 💨</h1>
+        <div className="min-h-screen grid place-items-center bg-verdeSuave-100">
+            <h1 className="text-3xl font-bold text-verdeSuave"> a bombar 💨</h1>
         </div>
     )
 }
