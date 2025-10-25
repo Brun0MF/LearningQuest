aec6b59837d8e20e14d69dc4aaeb79c6ee11cd46
Catarina Costa <catarinac.2005@gmail.com>
2025-10-25 22:10:51 +0100

    taillwindcss

--- frontend/src/views/pages/login/login.jsx
+++ frontend/src/views/pages/login/login.jsx
@@ -2,8 +2,8 @@
 
 const Login = () => {
     return (
-        <div>
-            LOGIN
+        <div className="min-h-screen grid place-items-center bg-blue-100">
+            <h1 className="text-3xl font-bold text-blue-600">Tailwind a bombar 💨</h1>
         </div>
     )
 }
