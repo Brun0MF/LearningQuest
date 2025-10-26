6a647c09d88286bf708d5cf4c0175715a5fe6e1d
Catarina Costa <catarinac.2005@gmail.com>
2025-10-26 01:31:15 +0000

    algo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./views/pages/home/home"
 import Login from "./views/pages/login/login"
 import Forgot from "./views/pages/login/forgotPass"
 import LoginLayout from "./views/pages/login/loginLayout"
+import CreateAccount from "./views/pages/login/createAccount"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route element={<LoginLayout />}>
           <Route path='/login' element={<Login />}></Route>
           <Route path="/forgot" element={<Forgot />}></Route>
+          <Route path="/create" element={<CreateAccount />}></Route>
         </Route>
         <Route element={''}>
           <Route path='/home' element={<Home />}></Route>
