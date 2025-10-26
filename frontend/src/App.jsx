493a9cc8b34652412367f2467409e3963fa5b625
MANODACOCA <dfso2013@gmail.com>
2025-10-26 01:29:27 +0000

    25% home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,33 @@
 import { Routes, Route } from "react-router-dom"
 import './App.css'
+
+//home
 import Home from "./views/pages/home/home"
+
+//login
 import Login from "./views/pages/login/login"
 import Forgot from "./views/pages/login/forgotPass"
 import LoginLayout from "./views/pages/login/loginLayout"
+import BaseLayout from "./views/pages/layout/baseLayout"
 
 function App() {
   return (
     <>
       <Routes>
+
+        {/*LOGIN*/}
         <Route element={<LoginLayout />}>
           <Route path='/login' element={<Login />}></Route>
           <Route path="/forgot" element={<Forgot />}></Route>
         </Route>
-        <Route element={''}>
+        {/*LOGIN*/}
+
+        {/*CONTEUDO DA PAGINA */}
+        <Route element={<BaseLayout />}>
           <Route path='/home' element={<Home />}></Route>
         </Route>
+        {/*CONTEUDO DA PAGINA */}
+
       </Routes>
     </>
   )
