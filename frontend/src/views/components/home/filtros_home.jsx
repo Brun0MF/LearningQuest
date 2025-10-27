c663d4733771367dacc668126e785a5e3d7b16fd
Catarina Costa <catarinac.2005@gmail.com>
2025-10-27 22:30:58 +0000

    classifica

--- frontend/src/views/components/home/filtros_home.jsx
+++ frontend/src/views/components/home/filtros_home.jsx
@@ -3,11 +3,11 @@
 const FiltrosHome = () => {
 
     return(
-        <div className="flex flex-row justify-between">
-            <select name="" id="" className="w-5/12 rounded-xl border border-verdeSuave-600 px-2 focus:border-verdeSuave focus:ring-2 focus:ring-verdeSuave outline-none transition-all">
+        <div className="flex flex-row gap-3 justify-between">
+            <select name="" id="" className="w-6/12 rounded-xl border border-verdeSuave-600 px-2 focus:border-verdeSuave focus:ring-2 focus:ring-verdeSuave outline-none transition-all">
                 <option value="">Categoria</option>
             </select>
-            <select name="" id="" className="w-5/12 rounded-xl border border-verdeSuave-600 px-2 focus:border-verdeSuave focus:ring-2 focus:ring-verdeSuave outline-none transition-all">
+            <select name="" id="" className="w-3/6 rounded-xl border border-verdeSuave-600 px-2 focus:border-verdeSuave focus:ring-2 focus:ring-verdeSuave outline-none transition-all">
                 <option value="">Tópico</option>
             </select>
             <button className="rounded-xl py-2 px-5 text-white font-medium bg-verdeSuave-600 hover:bg-verdeSuave-700">Aplicar</button>
