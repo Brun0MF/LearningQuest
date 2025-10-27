f4047bdafca97fb9fb91c4fef4fde830c9f3b76c
Catarina Costa <catarinac.2005@gmail.com>
2025-10-27 18:52:58 +0000

    feitos cards

--- frontend/src/views/components/home/filtros_home.jsx
+++ frontend/src/views/components/home/filtros_home.jsx
@@ -4,13 +4,13 @@ const FiltrosHome = () => {
 
     return(
         <div className="flex flex-row justify-between">
-            <select name="" id="">
-                <option value=""></option>
+            <select name="" id="" className="w-5/12 rounded-xl border border-verdeSuave-600 px-2 focus:border-verdeSuave focus:ring-2 focus:ring-verdeSuave outline-none transition-all">
+                <option value="">Categoria</option>
             </select>
-            <select name="" id="">
-                <option value=""></option>
+            <select name="" id="" className="w-5/12 rounded-xl border border-verdeSuave-600 px-2 focus:border-verdeSuave focus:ring-2 focus:ring-verdeSuave outline-none transition-all">
+                <option value="">Tópico</option>
             </select>
-            <button className="rounded-xl ">Aplicar</button>
+            <button className="rounded-xl py-2 px-5 text-white font-medium bg-verdeSuave-600 hover:bg-verdeSuave-700">Aplicar</button>
         </div>
     );
 }
