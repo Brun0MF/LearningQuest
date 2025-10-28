41ba4b617563615f8740aae9bb86ca4df0d8bf62
Catarina Costa <catarinac.2005@gmail.com>
2025-10-28 22:45:58 +0000

    xkczm

--- frontend/src/api/utilizadores.js
+++ frontend/src/api/utilizadores.js
@@ -1,4 +1,8 @@
 import axios from "axios";
 
-const url = 'http://localhost:8000/api/utilizadores';
+const url = 'http://localhost:8000/api/utilizadores/';
 
+export const getUtilizadores = async () => {
+    const response = await axios.get(url);
+    return response.data;
+}
