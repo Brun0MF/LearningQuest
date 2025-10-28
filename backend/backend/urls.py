9dca2568aabf5c13eba4d59a6be101f25d43fd52
Catarina Costa <catarinac.2005@gmail.com>
2025-10-28 16:06:10 +0000

    backend

---
+++ backend/backend/urls.py
@@ -0,0 +1,20 @@
+# backend/urls.py (urls do projeto)
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from core.views import (
+    UtilizadorViewSet, CategoriasViewSet, TopicosViewSet,
+    PontuacaoViewSet, PercursosViewSet
+)
+
+router = DefaultRouter()
+router.register(r'utilizadores', UtilizadorViewSet, basename='utilizador')
+router.register(r'categorias', CategoriasViewSet, basename='categorias')
+router.register(r'topicos', TopicosViewSet, basename='topicos')
+router.register(r'pontuacoes', PontuacaoViewSet, basename='pontuacao')
+router.register(r'percursos', PercursosViewSet, basename='percursos')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+]
---
+++ backend/core/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Utilizador, Categorias, Topicos, Pontuacao, Percursos
+
+class UtilizadorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Utilizador
+        fields = "__all__"
+
+class CategoriasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categorias
+        fields = "__all__"
+
+class TopicosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topicos
+        fields = "__all__"
+
+class PontuacaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pontuacao
+        fields = "__all__"
+
+class PercursosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Percursos
+        fields = "__all__"
