14467de18e3924b9b90640ebe751b3fc2c8eebb6
MANODACOCA <dfso2013@gmail.com>
2025-10-29 03:18:22 +0000

    ddd

--- frontend/src/api/utilizadores.js
+++ frontend/src/api/utilizadores.js
@@ -21,6 +21,7 @@ export const login = async (email, password) => {
 
         localStorage.setItem('access', response.data.access);
         localStorage.setItem('refresh', response.data.refresh);
+        localStorage.setItem('id_user', response.data.id_utilizador);
 
         return response.data;
     } catch (error) {
@@ -39,6 +40,42 @@ export const register = async (nome, email, password) => {
 
         localStorage.setItem('access', response.data.access);
         localStorage.setItem('refresh', response.data.refresh);
+        localStorage.setItem('id_user', response.data.id_utilizador);
+
+
+        return response.data;
+    } catch (error) {
+        console.error('Erro no registar nova conta:', error.response?.data || error.message);
+        throw error;
+    }
+};
+
+export const enviarCodigo = async (email) => {
+    try {
+        const response = await axios.post(`${url}codigo_email/`, {
+            email_utilizador: email
+        });
+
+        localStorage.setItem('email_user', email);
+        localStorage.setItem('codigo_2FA', response.data.codigo);
+
+        return response.data;
+    } catch (error) {
+        console.error('Erro no registar nova conta:', error.response?.data || error.message);
+        throw error;
+    }
+};
+
+export const mudarPassword = async (password) => {
+    try {
+        const email = localStorage.getItem('email_user');
+        const response = await axios.post(`${url}mudar_password/`, {
+            email_utilizador: email,
+            password_utilizador: password
+        });
+
+        localStorage.removeItem('email_user');
+        localStorage.removeItem('codigo_2FA');
 
         return response.data;
     } catch (error) {
