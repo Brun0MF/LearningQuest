06d735a27f0ec5831ce0699fdf75bd8fa8052f69
Catarina Costa <catarinac.2005@gmail.com>
2025-10-29 18:15:45 +0000

    cpommute de mersdasd

--- frontend/src/api/pontuacao.js
+++ frontend/src/api/pontuacao.js
@@ -10,3 +10,16 @@ export const getPontuacao_user_topico = (id_user, id_topico) => {
         console.log(e);
     }
 }
+
+export const updatePontuacao = (id_user, id_topico, pontos) => {
+    try {
+        const response = axios.post(`${url}update-user-topic/`, {
+            'id_utilizador': id_user,
+            'id_topico': id_topico,
+            'pontos': pontos
+        })
+        return response.data;
+    } catch (e) {
+        console.log(e);
+    }
+}
